49037d9a22643c7b187a4ed25d60c0e25d1cc16f
simge <simgesngn@gmail.com>
2020-12-18 11:16:51 +0300

    Initial commit

---
+++ Covid19/Covid19/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Covid19
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new string[] { "Shapes_Experimental" });
+            InitializeComponent();
+            Sharpnado.Tabs.Initializer.Initialize(loggerEnable: false, debugLogEnable: false);
+            Sharpnado.Shades.Initializer.Initialize(loggerEnable: false);
+            var nav = new NavigationPage(new LoginPage());
+            nav.BarBackgroundColor = Color.White;
+            nav.BackgroundColor = Color.White;
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
